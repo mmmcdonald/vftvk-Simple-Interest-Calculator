0242427f83a06bcbb633a95cefea924fbd6a3d29
mmmcdonald <mmcdonald@fonteva.com>
2021-06-20 18:46:22 +0000

    changes made from theia lab environment

--- script.js
+++ script.js
@@ -8,14 +8,14 @@ function compute()
 
         alert("Enter a positive number");
         document.getElementById("principal").focus();
+    } else {
+        var rate = document.getElementById("rate").value;
+        var years = document.getElementById("years").value;
+        var interest = principal * years * rate /100;
+        var year = new Date().getFullYear()+parseInt(years);
+        var amount = principal + interest;
+        document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>";
     }
-
-    var rate = document.getElementById("rate").value;
-    var years = document.getElementById("years").value;
-    var interest = principal * years * rate /100;
-    var year = new Date().getFullYear()+parseInt(years);
-    var amount = principal + interest;
-    document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>";
 }
 function updateRate() 
 {
