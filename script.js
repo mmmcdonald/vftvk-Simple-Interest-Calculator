aecad9054e610d9c4708ffc39143889b8b5e50e8
mmmcdonald <mmcdonald@fonteva.com>
2021-06-20 19:53:58 +0000

    changes made from theia lab environment

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
 function compute()
-{
-    //p = document.getElementById("principal").value;
-
-
+{ //function computes the data user enters into inputs. parseInt used below to derrive a correct calculation instead of a concactination. 
     var principal = parseInt(document.getElementById("principal").value);
+    //if statement used to generate alert in case of an entry <=0 in Amount.
     if(principal <= 0) {
 
         alert("Enter a positive number");
-        document.getElementById("principal").focus();
+        document.getElementById("principal").focus(); //focus included to put focus back on Amount input.
+        //else used below to avoid extraneous calculation and result output when 0 was entered.
     } else {
         var rate = document.getElementById("rate").value;
         var years = document.getElementById("years").value;
         var interest = principal * years * rate /100;
         var year = new Date().getFullYear()+parseInt(years);
+        //var amount added to compine principal and interest for updateRate function.
         var amount = principal + interest;
         document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>";
     }
 }
-function updateRate() 
+function updateRate() //displays calculated result from input.
 {
     var rateval = document.getElementById("rate").value;
     document.getElementById("rate_val").innerText=rateval;
